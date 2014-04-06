14ea4f49f31267f94db9fedeaed931eb4ec5cd63
notori0us <cbwcjw@gmail.com>
2014-04-06 16:30:43 -0400

    initial commit of a test website

---
+++ content/beep.md
@@ -0,0 +1,4 @@
+Title: My First Article
+Date: 2014/04/06 16:00
+
+Beep Boop beep boop beep!
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Chris Wallace'
+SITENAME = u'Chris Wallace'
+SITEURL = 'http://notoriouscw.com'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+        ('LinkedIn', 'https://www.linkedin.com/pub/christopher-wallace/66/830/b88'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
